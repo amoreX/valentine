5710002d05e2d875d2a106cb1544d761315b8537
N!hal <71655709+amoreX@users.noreply.github.com>
2024-02-08 09:09:28 +0530

    Add files via upload

---
+++ val.js
@@ -0,0 +1,18 @@
+document.getElementById("no").addEventListener("mouseenter", function(){
+    const pic=["cat1.jpg", "cat2.jpg", "cat3.jpg","cat4.jpg"];
+    const msg=["noo please","i will be sad :(","dont say noo","pweaseee"];
+    var x=Math.random()*1000;
+    var y=Math.random()*1000;
+    while (x>500|| y>600){
+        x=Math.random()*1000;
+        y=Math.random()*1000;
+    }
+    var randomImage = pic[Math.floor(Math.random() * pic.length)];
+            var randomMessage = msg[Math.floor(Math.random() * msg.length)];
+
+            document.getElementById("no").textContent = randomMessage;
+            document.getElementById("picture").src = "./Assets/" + randomImage;
+    
+    document.getElementById("no").style.left=x+"px";
+    document.getElementById("no").style.top=y+"px";
+})
